03e9c421c335e1835003ccdc72ea763bf42c37a4
Audrey <chipmanaudrey@gmail.com>
2024-06-24 23:53:16 -0700

    Work with arithmetic in Javascript

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) +1;
+
+
+let num3 = 22;
+let num4= 18;
+const mod = num3 % num4;
+
+const max = Math.max(1, 3, 7, 15, 17, 20);
